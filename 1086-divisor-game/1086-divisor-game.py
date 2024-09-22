e30174095f631c2e95cfd15a3e45ef77a0593355
natinecho <130102130+natinecho@users.noreply.github.com>
2024-09-22 09:17:58 +0300

    Time: 30 ms (84.81%), Space: 16.5 MB (38.37%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.py
+++ 1086-divisor-game/1086-divisor-game.py
@@ -1,39 +1,40 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        dp = {}
+        return n%2 == 0
+        # dp = {}
 
-        def divisors(num):
-            lim = int(sqrt(num))
-            arr = []
+        # def divisors(num):
+        #     lim = int(sqrt(num))
+        #     arr = []
 
-            for i in range(1,lim + 1):
-                if i != num and  num % i == 0:
-                    arr.append(i)
-                    if i != 1:
-                        arr.append(num // i)
+        #     for i in range(1,lim + 1):
+        #         if i != num and  num % i == 0:
+        #             arr.append(i)
+        #             if i != 1:
+        #                 arr.append(num // i)
 
-            return arr
+        #     return arr
 
-        def back(num,turn):
-            if num not in dp:
-                arr = divisors(num)
-                if turn == 1 and not arr:
-                    dp[num] = False
-                    return dp[num]
+        # def back(num,turn):
+        #     if num not in dp:
+        #         arr = divisors(num)
+        #         if turn == 1 and not arr:
+        #             dp[num] = False
+        #             return dp[num]
                 
-                if turn == 0 and not arr:
-                    dp[num] = True
-                    return dp[num]
+        #         if turn == 0 and not arr:
+        #             dp[num] = True
+        #             return dp[num]
 
 
-                temp = False
+        #         temp = False
 
-                for ni in arr:
-                    temp = temp or back(num - ni,1 - turn)
+        #         for ni in arr:
+        #             temp = temp or back(num - ni,1 - turn)
                 
-                dp[num] = temp
+        #         dp[num] = temp
 
-            return dp[num]
+        #     return dp[num]
 
-        back(n,1)
-        return dp[n]
+        # back(n,1)
+        # return dp[n]
