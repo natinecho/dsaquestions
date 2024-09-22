4b1d7e795d0774ea3e970a7128cee7d356ec247b
natinecho <130102130+natinecho@users.noreply.github.com>
2024-09-22 09:25:58 +0300

    Time: 45 ms (17.22%), Space: 16.6 MB (38.37%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.py
+++ 1086-divisor-game/1086-divisor-game.py
@@ -1,40 +1,29 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        return n%2 == 0
-        # dp = {}
+        dp = [False] *(n + 1)
 
-        # def divisors(num):
-        #     lim = int(sqrt(num))
-        #     arr = []
+        def divisors(num):
+            lim = int(sqrt(num))
+            arr = []
 
-        #     for i in range(1,lim + 1):
-        #         if i != num and  num % i == 0:
-        #             arr.append(i)
-        #             if i != 1:
-        #                 arr.append(num // i)
+            for i in range(1,lim + 1):
+                if i != num and  num % i == 0:
+                    arr.append(i)
+                    if i != 1:
+                        arr.append(num // i)
 
-        #     return arr
+            return arr
 
-        # def back(num,turn):
-        #     if num not in dp:
-        #         arr = divisors(num)
-        #         if turn == 1 and not arr:
-        #             dp[num] = False
-        #             return dp[num]
-                
-        #         if turn == 0 and not arr:
-        #             dp[num] = True
-        #             return dp[num]
+        for num in range(1,n + 1):
+            arr = divisors(num)
+            temp = False
+
+            for ni in arr:
+                if not dp[num - ni]:
+                    dp[num] = True
+                    break
 
 
-        #         temp = False
 
-        #         for ni in arr:
-        #             temp = temp or back(num - ni,1 - turn)
-                
-        #         dp[num] = temp
 
-        #     return dp[num]
-
-        # back(n,1)
-        # return dp[n]
+        return dp[n]
