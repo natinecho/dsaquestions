66cc375482e7facd18add77401f6e814f0913535
natinecho <130102130+natinecho@users.noreply.github.com>
2024-10-01 10:06:58 +0300

    Time: 90 ms (84.95%), Space: 18 MB (36.31%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -1,14 +1,27 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        dp = [[float('inf')] * (k + 2) for _ in range(n)]
-        
-        for i in range(k + 2):
-            dp[src][i] = 0
-        
-        for s in range(1, k + 2):
-            for u, v, price in flights:
-                dp[v][s] = min(dp[v][s], dp[u][s - 1] + price)
-        
-        result = min(dp[dst][s] for s in range(1, k + 2))
-        
-        return -1 if result == float('inf') else result
+        graph = defaultdict(list)
+
+        for u,v,w in flights:
+            graph[u].append((v,w))
+            # graph[v].append((u,w))
+
+        que = [(0,0,src)]
+        visited = {}
+
+        while que:
+            price,level,node = heappop(que)
+            if node == dst and level <= k + 1:
+                return price
+
+            if level > k:
+                continue
+
+            if node not in visited or visited[node] > level:
+                visited[node] = level
+
+                for ni,wh in graph[node]:
+                    heappush(que,(price + wh , level + 1,ni))
+
+
+        return -1
