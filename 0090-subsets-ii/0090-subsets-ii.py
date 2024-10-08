5b80a3f17fca9b3e427af4d625ded77aa389da23
natinecho <130102130+natinecho@users.noreply.github.com>
2024-10-08 18:37:25 +0300

    Time: 39 ms (57.31%), Space: 16.6 MB (82.87%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        path = []
+
+        sets = set()
+
+        def backTrack(i):
+
+            sets.add(tuple(sorted(path.copy())))
+
+            if len(path) == len(nums):
+                return 
+
+            for j in range(i,len(nums)):
+                path.append(nums[j])
+                backTrack(j+1)
+                path.pop()
+        
+        backTrack(0)
+
+        return list(sets)
