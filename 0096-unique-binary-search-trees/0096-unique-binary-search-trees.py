2eb7d8a5281f1dd60975b1d201b86c0905d92fec
natinecho <130102130+natinecho@users.noreply.github.com>
2024-10-19 16:21:53 +0300

    Time: 0 ms (100%), Space: 16.6 MB (7.01%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,56 +1,53 @@
 class Solution:
-    def numTrees(self, n: int) -> int:
-        
-        # def back(arr):
-        #     tt = tuple(arr)
-        #     if tt not in dp:
-        #         if len(arr) == 1:
-        #             dp[tt] = 1
-        #         else:
-        #             temp = 0
-        #             for i in range(len(arr)):
-        #                 zz = back(arr[:i]) + back(arr[i + 1:])
-        #                 temp += zz
-                        
-        #             dp[tt] = temp + 1
-
-        #     return dp[tt]
-
-        # dp = {}
-        # arr = [i for i in range(1,n + 1)]
-
-        # ans = back(arr)
-        # print(dp)
-        # return ans
-        
-        def back(val):
-            if val not in dp:
-                if val <= 2  :
-                    dp[val] = val
-                    return val
+    def numTrees(self, n: int) -> int:     
+        # def back(val):
+        #     if val not in dp:
+        #         if val <= 2  :
+        #             dp[val] = val
+        #             return val
                 
-                temp = 0
-                for i in range(val):
-                    ll = 1
-                    rr = 1
-                    if i != 0:
-                        ll = back(i) 
+        #         temp = 0
+        #         for i in range(val):
+        #             ll = 1
+        #             rr = 1
+        #             if i != 0:
+        #                 ll = back(i) 
                         
-                    if ( val - (1 + i)) != 0:
-                        rr = back(val - (1 + i))
+        #             if ( val - (1 + i)) != 0:
+        #                 rr = back(val - (1 + i))
 
-                    temp += (ll * rr)
+        #             temp += (ll * rr)
 
-                dp[val] = temp
+        #         dp[val] = temp
 
-            return dp[val]
+        #     return dp[val]
 
             
-        dp = {}
+        # dp = {}
 
-        ans = back(n)
-        return ans
+        # ans = back(n)
+        # return ans
 
-                
+        dp = [1]*(n + 1)
+
+        for val in range(2,n + 1):
+
+            temp = 0
+            for i in range(val):
+                ll = 1
+                rr = 1
+                if i != 0:
+                    ll = dp[i]
+                    
+                if ( val - (1 + i)) != 0:
+                    rr = dp[val - (1 + i)]
+
+                temp += (ll * rr)
+
+            dp[val] = temp
+
+
+        return dp[n]
+
 
                     
