5f1a0f911e415899b4f4956391512ff4e4ba8ffa
natinecho <130102130+natinecho@users.noreply.github.com>
2024-10-24 09:48:28 +0300

    Time: 2 ms (93.72%), Space: 16.7 MB (9.31%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,4 +1,39 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.index(needle) if needle in haystack else -1
+        poww = [1]
+        MOD = 10 ** 9 + 7
+        n = len(haystack)
+        m = len(needle)
+
+        if m > n:
+            return -1
+
+
+        for i in range(m):
+            poww.append((poww[-1] * 27) % MOD )
         
+        summ = 0 
+
+        for  i in range(m):
+            summ = ((summ *27) +  (ord(needle[i]) - 96)) % MOD
+
+        temp = 0
+        for  i in range(m):
+            temp = ((temp *27) +  (ord(haystack[i]) - 96)) % MOD
+
+        if temp == summ:
+            if haystack[:m] == needle:
+                return 0
+
+        l = 0
+
+        for i in range(m,n):
+            temp -= ((ord(haystack[l]) - 96)* (poww[m - 1]))% MOD
+            temp = ((temp *27) +  (ord(haystack[i]) - 96)) % MOD
+            l += 1
+            
+            if temp == summ:
+                if haystack[l:i + 1] == needle:
+                    return l
+
+        return -1
