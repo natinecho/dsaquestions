539540994356087adf07f9bf14db43b44ec92040
natinecho <130102130+natinecho@users.noreply.github.com>
2024-11-08 15:52:14 +0300

    Time: 4 ms (57.45%), Space: 35.6 MB (9.55%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.py
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.py
@@ -2,23 +2,11 @@ class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
 
         def help(nums,n):
-            arr = [0]*32
+            val = 0
+            if n % 2 != 0:
+                for num in nums:
+                    val ^= num
 
-            for num in nums:
-                for i in range(32):
-                    arr[i] += 1 if (num & (1 << i)) else 0
-            
-            # print(arr)
+            return val
 
-            for i in range(32):
-                arr[i] = (arr[i] * n)%2
-
-            ans = 0
-            
-            for i in range(32):
-                ans |= (arr[i] << i)
-
-            return ans
-
-            
-        return help(nums1, len(nums2)) ^ help(nums2, len(nums1))
+        return help(nums1,len(nums2)) ^ help(nums2,len(nums1))
