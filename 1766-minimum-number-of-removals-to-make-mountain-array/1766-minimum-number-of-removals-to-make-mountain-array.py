bdbb1652be1a72ba74083c063e4e32092b82fad3
natinecho <130102130+natinecho@users.noreply.github.com>
2024-11-28 19:02:20 +0300

    Time: 19 ms (86.76%), Space: 17.4 MB (9.4%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.py
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.py
@@ -1,27 +1,30 @@
-
-
 class Solution:
     def minimumMountainRemovals(self, nums: List[int]) -> int:
         n = len(nums)
         
-
-        front = [1] * n
-        for i in range(1, n):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    front[i] = max(front[i], front[j] + 1)
+        front = [0] * n
+        lis = []
+        for i in range(n):
+            pos = bisect_left(lis, nums[i])
+            if pos == len(lis):
+                lis.append(nums[i])
+            else:
+                lis[pos] = nums[i]
+            front[i] = pos + 1
         
-
-        back = [1] * n
-        for i in range(n - 2, -1, -1):
-            for j in range(n - 1, i, -1):
-                if nums[i] > nums[j]:
-                    back[i] = max(back[i], back[j] + 1)
+        back = [0] * n
+        lis = []
+        for i in range(n - 1, -1, -1):
+            pos = bisect_left(lis, nums[i])
+            if pos == len(lis):
+                lis.append(nums[i])
+            else:
+                lis[pos] = nums[i]
+            back[i] = pos + 1
         
-
-        max_mountain = 0
+        maxx = 0
         for i in range(1, n - 1):  
             if front[i] > 1 and back[i] > 1: 
-                max_mountain = max(max_mountain, front[i] + back[i] - 1)
+                maxx = max(maxx, front[i] + back[i] - 1)
         
-        return n - max_mountain
+        return n - maxx
