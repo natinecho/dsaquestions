7fccdd620b016808a99e34fd54c9807521b1a1d8
Natnael Necho <130102130+natinecho@users.noreply.github.com>
2025-04-23 15:02:50 +0300

    Time: 51 ms (33.33%), Space: 23.2 MB (44.88%) - LeetHub

---
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -0,0 +1,17 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.dq=deque()
+        self.tot=3000
+
+    def ping(self, t: int) -> int:
+        self.dq.append(t)
+        temp=t-self.tot
+
+        # if temp<0:
+        #     return len(self.dq)
+            
+        while self.dq and temp>self.dq[0]:
+            self.dq.popleft()
+        
+        return len(self.dq)
