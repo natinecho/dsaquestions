197b9ea8e3ea77eda9dbc60771dfa1bf6860125d
Natnael Necho <130102130+natinecho@users.noreply.github.com>
2025-06-24 22:47:58 +0300

    Time: 7300 ms (5.16%), Space: 18.3 MB (19.35%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.py
+++ 0282-expression-add-operators/0282-expression-add-operators.py
@@ -1,54 +1,25 @@
 class Solution:
     def addOperators(self, num: str, target: int):
-        res = []
+        answer = []
+        def back( position, expression):
+            #basecase
+            if len(num) == position:
+                if eval(expression) == target:
+                    answer.append(expression) 
+                return 
 
-        def backtrack(expr, pos):
-            if pos == len(num):
-                if eval(expr) == target:
-                    res.append(expr)
-
-            
-            for i in range(pos + 1, len(num) + 1):
-                temp = num[pos:i]
-                if len(temp) > 1 and temp[0] == '0':
+            for i in range(position + 1, len(num) + 1):
+                temp = num[position: i]
+                if len(temp) > 1 and temp[0] == "0":
                     continue
-
-                if pos == 0:
-                    backtrack(temp, i)
+                if position == 0:
+                    back( i,temp)
                 else:
-                    backtrack(expr + '+' + temp, i)
-                    backtrack(expr + '-' + temp, i)
-                    backtrack(expr + '*' + temp, i)
-
-        backtrack("", 0)
-        return res
-
-    # def addOperators(self, num: str, target: int) -> List[str]:
-    #     res = []
-
-    #     def backtrack(expr, pos, prev, val):
-    #         if pos == len(num):
-    #             if val == target:
-    #                 res.append(expr)
-    #             return
+                    back( i,expression + "+" + temp) 
+                    back( i,expression + "*" + temp) 
+                    back( i,expression + "-" + temp) 
+            return 
+        back(0,"")
+        return answer
             
-    #         for i in range(pos + 1, len(num) + 1):
-    #             temp = num[pos:i]
-    #             # Skip numbers with leading zeros
-    #             if len(temp) > 1 and temp[0] == '0':
-    #                 continue
-    #             curr_num = int(temp)
 
-    #             if pos == 0:
-    #                 # First number, pick without any operator
-    #                 backtrack(temp, i, curr_num, curr_num)
-    #             else:
-    #                 # Addition
-    #                 backtrack(expr + '+' + temp, i, curr_num, val + curr_num)
-    #                 # Subtraction
-    #                 backtrack(expr + '-' + temp, i, -curr_num, val - curr_num)
-    #                 # Multiplication
-    #                 backtrack(expr + '*' + temp, i, prev * curr_num, val - prev + prev * curr_num)
-
-    #     backtrack("", 0, 0, 0)
-    #     return res
