197a42e6b898757f19b6537d5ae524de99ce44f5
Natnael Necho <130102130+natinecho@users.noreply.github.com>
2025-09-10 19:01:37 +0300

    Time: 19 ms (8.18%), Space: 18.6 MB (13.68%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,36 +1,24 @@
-class UnionFind:
-    def __init__(self,size):
-        self.root = {i:i for i in range(1,size + 1)}
-        self.size = [0] * (size + 1)
-
-    def find(self, x):
-        if x == self.root[x]:
-            return x
-        self.root[x] = self.find(self.root[x])
-        return self.root[x]
-
-    def union(self, x, y):
-     
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            if self.size[rootX] > self.size[rootY]:
-                self.root[rootY] = rootX
-                self.size[rootX] += self.size[rootY]
-            else:
-                self.root[rootX] = rootY
-                self.size[rootY] += self.size[rootX]
-        else:
-            return False
-        
-        return True
-
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        obj = UnionFind(len(edges))
-        for i,j in edges:
+        graph = defaultdict(list)
+
+        def hasCycle(node,prev):
+            if node in visited:
+                return True
             
-            if not obj.union(i,j):
-                return [i,j]
-            
+            visited.add(node)
+            for n in graph[node]:
+                if n != prev:
+                    if hasCycle(n,node):
+                        return True
+            return False 
+
+        for a,b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+            visited = set()
+            if hasCycle(a,-1):
+                return [a,b]
         
+
+        return []
